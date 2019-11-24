74ab7a1f7f6f17019ca81fa58ccf5e58a814cd7e
Royal Bhati <royal240596@gmail.com>
2019-11-24 05:49:42 -0500

    :lipstick: improved cli

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 #!/usr/bin/env node
 const path = require("path");
 const ncp = require("ncp").ncp;
+const Listr = require("listr");
+const { projectInstall } = require("pkg-install");
+const chalk = require("chalk");
 
-(() => {
+const copyFiles = async () => {
   const cwd = process.cwd();
   ncp(path.resolve(__dirname, "..") + `/client`, cwd, function(err) {
     if (err) {
       return console.error(err);
     }
-    console.log("successfully created");
-    console.log("installing node_modules...........");
-    const { spawnSync } = require("child_process"),
-      ls = spawnSync("npm", ["install"]);
-
-    console.log(`stderr: ${ls.stderr.toString()}`);
-    console.log(`stdout: ${ls.stdout.toString()}`);
+    return new Promise((resolve, reject) => {
+      resolve(1);
+    });
   });
-})();
+};
+
+const tasks = new Listr([
+  {
+    title: "Copying project files",
+    task: () => copyFiles()
+  },
+  {
+    title: "Installing dependencies",
+    task: () => projectInstall()
+  }
+]);
+tasks.run().then(() => {
+  console.log("%s Project ready", chalk.green.bold("DONE"));
+});
